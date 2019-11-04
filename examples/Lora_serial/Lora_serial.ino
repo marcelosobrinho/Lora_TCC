ae099b54bbb10e54cdd1933ac31594697b60537f
Marcelo Sobrinho <sobrinho11191@gmail.com>
2019-11-03 22:58:45 -0400

    FELIZ DE MAIS, EXEMPLO RODANDO TOP, :D

--- examples/Lora_serial/Lora_serial.ino
+++ examples/Lora_serial/Lora_serial.ino
@@ -1,16 +1,26 @@
 #include <Lora_serial.h>
-Conexao conexaolora(2,3,9600);
-
-
+Conexao conexaolora(2, 3, 5, 9600);
+double date_s = 10.999;
+double date_s1 = 11.145;
+double date_s2 = 10.55;
+double date_s3 = 14;
+double date_s4 = 17;
+double date_s5 = 40;
+double date_s6 = 40;
 void setup() {
-conexaolora.quant_disp(10);
-
 
 }
 
 void loop() {
- conexaolora.iniciar(); 
- 
- 
+
+  conexaolora.empacotar(date_s1);
+  conexaolora.empacotar(date_s2);
+  conexaolora.empacotar(date_s3);
+  conexaolora.empacotar(date_s4);
+  conexaolora.empacotar(date_s5);
+  conexaolora.empacotar(date_s6);
+  conexaolora.iniciar_trans();
+
+
 
 }
